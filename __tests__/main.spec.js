61ec81a72b0833d9e7c6de759d10bfab50feb169
Linkoff <linkov.ea@yandex.ru>
2026-09-25 19:22:57 +0300

    test: add e2e test for tic-tac-toe game

--- __tests__/main.spec.js
+++ __tests__/main.spec.js
@@ -1,9 +1,40 @@
-// @ts-check
+// __tests__/main.spec.yml
 
+// Подключаем стили для vitest-preview
+import "@hexlet/tic-tac-toe/public/style.css";
 import { test, expect } from "vitest";
+import { screen } from "@testing-library/dom";
+import userEvent from "@testing-library/user-event";
+
+// Импортируем игру
+import { TicTacToe } from "@hexlet/tic-tac-toe";
+import { debug } from "vitest-preview";
 
 test("main", async () => {
-  document.body.innerHTML = "<h1>Hexlet</h1>";
+  // Объект для выполнения действий над элементами
+  const user = userEvent.setup();
 
-  expect(document.body).toHaveTextContent("Hexlet");
+  // Заполняем document.body
+  // Игра "разворачивает" себя в переданный элемент
+  // Вызов метода start вешает обработчики
+  const game = new TicTacToe(document.body);
+  game.start();
+   debug();
+
+  // Выбираем нужные элементы
+  // Находим поля для ввода имен игроков
+  const input1 = screen.getByLabelText("Player 1");
+  const input2 = screen.getByLabelText("Player 2");
+
+  // Выполняем необходимые действия
+  // Вводим имена пользователей
+  await user.type(input1, "user 1");
+  await user.type(input2, "user 2");
+
+  // Отправляем форму
+  const submitButton = screen.getByText("Start Game");
+  await user.click(submitButton);
+
+  // Проверяем результат
+  expect(document.body).toHaveTextContent("user 1, you are up!");
 });
